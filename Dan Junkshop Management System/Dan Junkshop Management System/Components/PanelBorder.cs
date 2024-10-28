e6d7128accb1809db4bd994e8851024792bb3fc2
Adrianne Lang <137845861+AdrianneVilla@users.noreply.github.com>
2024-10-28 15:00:57 +0800

    Add files via upload

---
+++ Dan Junkshop Management System/Dan Junkshop Management System/Components/PanelBorder.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace WindowsFormsApp2.CustomButton
+{
+    public class PanelBorder : Panel
+    {
+        private int borderRadius = 30;
+        private Color color = Color.BurlyWood;
+
+        public PanelBorder()
+        {
+            this.BackColor = Color.White;
+            this.ForeColor = Color.Black;
+            this.Size = new Size(350, 200);
+
+        }
+
+        //Properties
+        public int BorderRadius
+        {
+            get
+            {
+                return borderRadius;
+            }
+            set
+            {
+                borderRadius = value;
+                this.Invalidate();
+            }
+        }
+        public Color Color
+        {
+            get
+            {
+                return color;
+            }
+            set
+            {
+                color = value;
+                this.Invalidate();
+            }
+        }
+        public override string Text
+        {
+            get
+            {
+                return base.Text;
+            }
+            set
+            {
+
+            }
+        }
+
+        private GraphicsPath getFigurePath(RectangleF rect, float radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+
+            path.AddArc(rect.X, rect.Y, radius, radius, 180, 90);
+            path.AddArc(rect.Width - radius, rect.Y, radius, radius, 270, 90);
+            path.AddArc(rect.Width - radius, rect.Height - radius, radius, radius, 0, 90);
+            path.AddArc(rect.X, rect.Height - radius, radius, radius, 90, 90);
+            path.CloseFigure();
+
+            return path;
+        }
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+
+            pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            RectangleF rectSuface = new RectangleF(0, 0, this.Width, this.Height);
+
+
+            if (borderRadius > 2)
+            {    // rounded button
+
+                using (GraphicsPath pathSurface = getFigurePath(rectSuface, borderRadius))
+                using (Pen penSurface = new Pen(this.Parent.BackColor, 2))
+                {
+
+                    this.Region = new Region(pathSurface);
+                    pevent.Graphics.DrawPath(penSurface, pathSurface);
+
+                }
+
+            }
+            else
+            {
+                //button surface
+                this.Region = new Region(rectSuface);
+
+                //button border
+
+
+
+            }
+
+        }
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            this.Parent.BackColorChanged += new EventHandler(Container_BackColorChanged);
+        }
+
+        private void Container_BackColorChanged(object sender, EventArgs e)
+        {
+            if (this.DesignMode)
+            {
+                this.Invalidate();
+            }
+        }
+        private void Button_Resize(object sender, EventArgs e)
+        {
+            if (borderRadius > this.Height)
+            {
+                borderRadius = this.Height;
+
+            }
+        }
+
+    }
+}
---
+++ Dan Junkshop Management System/Dan Junkshop Management System/Inventory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dan_Junkshop_Management_System
+{
+    public partial class Inventory : UserControl
+    {
+        public Inventory()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Inventory_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public static implicit operator Inventory(HeaderWText v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
